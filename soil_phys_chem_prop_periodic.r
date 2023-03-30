38be3ec3c602e1294a4ee0213aacdf8b225bdaa2
Mark A. Miller <MAM@lbl.gov>
2023-03-30 06:36:58 -0400

    match soil_phys_chem_prop_periodic_without_asking variable names
    match soil_phys_chem_prop_periodic_without_asking variable names

--- soil_phys_chem_prop_periodic.r
+++ soil_phys_chem_prop_periodic.r
@@ -3,4 +3,4 @@ library("feather")
 
 
 soil_phys_chem_prop_periodic_without_asking <- loadByProduct(dpID = "DP1.10086.001", check.size = FALSE)
-soil_phys_chem_prop_periodic$sls_soilpH[1:3,1:14]
+soil_phys_chem_prop_periodic_without_asking$sls_soilpH[1:3,1:14]
