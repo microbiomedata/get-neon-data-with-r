4ad128ed728489b7a9db3a0343712b4b3eba3228
Mark A. Miller <MAM@lbl.gov>
2023-03-30 09:59:18 -0400

    should write and commit a feather file

--- soil_phys_chem_prop_periodic.r
+++ soil_phys_chem_prop_periodic.r
@@ -1,6 +1,9 @@
 library("neonUtilities")
 library("feather")
 
+path = 'sls_soilpH.feather'
 
 soil_phys_chem_prop_periodic_without_asking <- loadByProduct(dpID = "DP1.10086.001", check.size = FALSE)
 soil_phys_chem_prop_periodic_without_asking$sls_soilpH[1:3,1:14]
+
+write_feather(soil_phys_chem_prop_periodic_without_asking$sls_soilpH, path)
